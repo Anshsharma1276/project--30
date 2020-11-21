720541fc338ed6f3e48c51af0179cf4c3fc38800
Anshsharma1276 <72375991+Anshsharma1276@users.noreply.github.com>
2020-11-21 13:49:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,124 +1,108 @@
-// various constatnts for the physics engine
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Constraint = Matter.Constraint;
+const Constraint=Matter.Constraint 
 
 var engine, world;
-var knightImg, knight;
-var boxes = [];
-var x = 0;
+var polygonImage;
+var score=0;
 
-// Adding GameState
-var gameState = "Aim";
-
-// loading knight animation
 function preload() {
-    knightImg = loadAnimation("Assets/Knight/Idle (1).png", "Assets/Knight/Idle (2).png", "Assets/Knight/Idle (3).png", "Assets/Knight/Idle (4).png", "Assets/Knight/Idle (5).png")
-    bg = loadImage("Assets/bg.jpg");
+polygonImage=loadImage("polygon.png") 
 }
-
-
-function setup() {
-    createCanvas(2500, 800);
-
+function setup(){
+    var canvas = createCanvas(2000,500);
     engine = Engine.create();
     world = engine.world;
+stand1=new Stand(390,300,250,10)
+stand2=new Stand(700,200,200,10)
 
-    // creating a ground
-    ground = new Ground(width);
-    // creating a sling
-    sling = new Sling(150, 350);
 
-    // creating a semi-pyramid of boxes using array
-    for (var i = 1; i <= 4; i++) {
-        for (var j = 1; j <= i; j++) {
-            x = 184 * j;
-            var box = new Box(1500 + x, 250);
-            boxes.push(box);
-        }
-    }
-    for (var i = 1; i <= 3; i++) {
-        for (var j = 1; j <= i; j++) {
-            x = 184 * j;
-            var box = new Box(600 + x, 325);
-            boxes.push(box);
-        }
-    }
 
-    platform1 = new Platform(2000, 500, 184 * 4);
-    platform2 = new Platform(1000, 650, 184 * 3);
 
-    // creating a knight
-    knight = createSprite(120, 580);
-    knight.addAnimation("knightImg", knightImg);
-    knight.scale = 0.5;
-
-    // creating the fireball
-    ball = new Ball();
-
-    // creating a rubber band
-    rubber = new Rubber(ball.body, {
-        x: 190,
-        y: 375
-    });
-
-    Engine.run(engine);
+    box1 = new Box(300,275,30,40);
+    box2 = new Box(330,275,30,40);
+    box3 = new Box(360,275,30,40)
+    box4 = new Box(390,275,30,40)
+    box5 = new Box(420,275,30,40)
+    box6 = new Box(450,275,30,40)
+    Box7 = new Box(480,275,30,40);
+    Box8 = new Box(330,235,30,40);
+    Box9 = new Box(360,235,30,40);
+    Box10 = new Box(390,235,30,40);
+    Box11 = new Box(420,235,30,40);
+    Box12 = new Box(450,235,30,40);
+    box13 = new Box(360,195,30,40);
+    Box14 = new Box(390,195,30,40);
+    Box15 = new Box(420,195,30,40);
+    box16 = new Box(390,155,30,40)
+    box17 = new Box(640,175,30,40)
+    box18 = new Box(670,175,30,40);
+    box19 = new Box(700,175,30,40);
+    box20 = new Box(730,175,30,40);
+    box21 = new Box(760,175,30,40);
+    box22 = new Box(670,135,30,40);
+    box23 = new Box(700,135,30,40);
+    box24 = new Box(730,135,30,40);
+    box25 = new Box(700,95,30,40);
+    
+    
+    ground = new Ground()
+    polygon=Bodies.circle(50,200,20)
+    World.add(world,polygon)
+    
+sling1=new Slingshot(this.polygon,{x:100,y:200});
 }
 
-function draw() {
-    push();
-    tint(243, 190, 122);
-    background(bg);
-    pop();
+function draw(){
+   
+    background("skyblue");
     Engine.update(engine);
+    textSize(25)
+    text(" score: " + score, 300,30)
 
-    // displaying knight
-    drawSprites();
-    push();
-    tint(243, 190, 122);
-    ground.show();
-    // displaying the elements of array
-    platform1.show();
-    platform2.show();
-    pop();
-    for (var t = 0; t < boxes.length; t++) {
-        if (boxes[t].body.speed < 25) {
-            boxes[t].show();
-        } else {
-            boxes[t].invisible(boxes[t].body);
-        }
-    };
-
-    ball.show();
-    rubber.show();
-    sling.show();
-
-    if (gameState != "launched") {
-        if (mouseIsPressed) {
-            Body.setPosition(ball.body, {
-                x: mouseX,
-                y: mouseY
-            });
-        }
-    }
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    Box7.display();
+    Box8.display();
+    Box9.display();
+    Box10.display();
+    Box11.display();
+    Box12.display();
+    box13.display();
+    Box14.display();
+    Box15.display();
+    box16.display();
+    box17.display();
+    box18.display();
+    box19.display();
+    box20.display();
+    box21.display();
+    box22.display();
+    box23.display();
+    box24.display();
+    box25.display();
+    stand1.display();
+    stand2.display();
+    ground.display()
+    imageMode(CENTER)
+    image(polygonImage,polygon.position.x ,polygon.position.y,40,40)
+    sling1.display();
+    
 }
-
-// relesing the ball from the sling
+function mouseDragged() {
+Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY})
+}
 function mouseReleased() {
-    rubber.rubber.bodyA = null;
-    gameState = "launched";
+sling1.fly()
+}
+function keyPressed(){
+ if (keyCode==32){
+     sling1.attach(this.polygon)
+ }   
 }
 
-// reloading the sling with a ball
-function keyPressed() {
-    if (keyCode == 32) {
-        rubber.rubber.bodyA = ball.body;
-        Body.setPosition(ball.body, {
-            x: 150,
-            y: 350
-        });
-        gameState = "Aim";
-    }
-}
